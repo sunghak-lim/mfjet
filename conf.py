74882430b1debf3b98efd069d18cef4ec90757bb
sunghak.lim <sunghak.lim@rutgers.edu>
2024-05-22 22:56:53 -0400

    doc

---
+++ conf.py
@@ -0,0 +1,122 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'mfjet'
+copyright = '2024, Sung Hak Lim'
+author = 'Sung Hak Lim'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = False
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# autodoc setting
+autodoc_member_order = 'bysource'
+autodoc_default_options = {
+    'show-inheritance': True,
+    'members': True,
+    'inherited-members': True,
+    'member-order': 'bysource',
+    #'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+#html_static_path = []
+
+
+# hack for accessing module path
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../mfjet'))
+
+
+# ClassDocumenter.add_directive_header uses ClassDocumenter.add_line to
+#   write the class documentation.
+# We'll monkeypatch the add_line method and intercept lines that begin
+#   with "Bases:".
+# In order to minimize the risk of accidentally intercepting a wrong line,
+#   we'll apply this patch inside of the add_directive_header method.
+
+from sphinx.ext.autodoc import ClassDocumenter, _
+
+add_line = ClassDocumenter.add_line
+line_to_delete = _(u'Bases: %s') % u':class:`object`'
+
+def add_line_no_object_base(self, text, *args, **kwargs):
+    if text.strip() == line_to_delete:
+        return
+
+    add_line(self, text, *args, **kwargs)
+
+add_directive_header = ClassDocumenter.add_directive_header
+
+def add_directive_header_no_object_base(self, *args, **kwargs):
+    self.add_line = add_line_no_object_base.__get__(self)
+
+    result = add_directive_header(self, *args, **kwargs)
+
+    del self.add_line
+
+    return result
+
+ClassDocumenter.add_directive_header = add_directive_header_no_object_base
+
+
