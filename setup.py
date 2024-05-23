11aa58b5a01014a7a5f389ec7717a97898a161c1
sunghak.lim <sunghak.lim@rutgers.edu>
2024-05-23 17:23:03 -0400

    modernized setuptools

--- setup.py
+++ setup.py
@@ -1,29 +1,12 @@
 from os import path
-from setuptools import find_packages, setup
+from setuptools import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+# get version
 version_file = path.join(path.abspath(path.dirname(__file__)), 'mfjet', 'version.py')
 with open(version_file, 'r') as file:
     exec(file.read())
 
+# fill out dynamic entries
 setup(
-    name="mfjet",
-    version=__version__,
-    description="Minkowski functionals in jet physics",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url="https://github.com/sunghak-lim/mfjet",
-    download_url = 'https://github.com/',
-    author="Sung Hak Lim",
-    packages=find_packages(exclude=["tests"]),
-    license="",
-    install_requires=[
-        "numpy",
-        "scipy",
-        "shapely>=2",
-    ],
-    dependency_links=[],
+    version = __version__
 )
